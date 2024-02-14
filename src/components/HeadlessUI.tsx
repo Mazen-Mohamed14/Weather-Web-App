8cebf3b95e9b97035a042761af5aee3e4667ee91
Mazen Mohamed <mazenmezomohammed@gmail.com>
2024-02-14 23:18:25 +0200

    feat: add link to city weather page

--- src/components/HeadlessUI.tsx
+++ src/components/HeadlessUI.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, Fragment } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/16/solid";
+import { Link } from "react-router-dom";
 
 export type City = {
   name: string;
@@ -62,36 +63,44 @@ function HeadlessUI() {
                 </div>
               ) : (
                 filteredCity.map(({ name }, index) => (
-                  <Combobox.Option
+                  <Link
                     key={index}
-                    className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? "bg-teal-600 text-white" : "text-gray-900"
-                      }`
-                    }
-                    value={name}
+                    to={`/city/${encodeURIComponent(name)}`}
+                    className="block"
                   >
-                    {({ selected, active }) => (
-                      <>
-                        <span
-                          className={`block truncate ${
-                            selected ? "font-medium" : "font-normal"
-                          }`}
-                        >
-                          {name}
-                        </span>
-                        {selected ? (
+                    <Combobox.Option
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                          active ? "bg-teal-600 text-white" : "text-gray-900"
+                        }`
+                      }
+                      value={name}
+                    >
+                      {({ selected, active }) => (
+                        <>
                           <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? "text-white" : "text-teal-600"
+                            className={`block truncate ${
+                              selected ? "font-medium" : "font-normal"
                             }`}
                           >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                            {name}
                           </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Combobox.Option>
+                          {selected ? (
+                            <span
+                              className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                active ? "text-white" : "text-teal-600"
+                              }`}
+                            >
+                              <CheckIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Combobox.Option>
+                  </Link>
                 ))
               )}
             </Combobox.Options>
