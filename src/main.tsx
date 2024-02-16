be02dc5b269429f263119292a4b8d73ab4f63100
Mazen Mohamed <mazenmezomohammed@gmail.com>
2024-02-16 22:24:06 +0200

    feat: add routes for main page and city page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Weather from "./pages/Weather.tsx";
+import HeadlessUI from "./components/HeadlessUI.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    children: [
+      { path: "/", element: <HeadlessUI></HeadlessUI> },
+      { path: "/city/:cityName", element: <Weather></Weather> },
+    ],
   },
 ]);
 
